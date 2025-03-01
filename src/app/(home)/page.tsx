e80b1dbc344cf481ee28098308a9562e5e5c3413
crucials <ensueness@gmail.com>
2025-03-01 18:02:31 +0500

    implement hero section with animations using framer motion

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,23 +1,29 @@
-import waveformIllustration from '@/assets/images/waveform-illustration.svg'
-
-import Image from 'next/image'
+import { PlayIcon } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import WaveformIllustration from '@/app/(home)/waveform-illustration'
 
 export default function Home() {
     return (
-        <main className="p-10">
-            <h1 className="scroll-m-20 text-4xl text-center font-extrabold tracking-tight lg:text-5xl">
-                Listen two AI models talk to each other
-            </h1>
+        <main>
+            <section className="relative pb-10">
+                <div className="bg-background/50 backdrop-blur-[3px] absolute w-full h-full top-0 left-0 z-20"></div>
 
-            <p className="leading-7 [&:not(:first-child)]:mt-6 text-center">
-                And other fun AI-related things
-            </p>
+                <div className="relative p-6 mb-6 z-30 flex items-center justify-center flex-col">
+                    <h1 className="scroll-m-20 text-4xl text-center font-extrabold tracking-tight lg:text-5xl">
+                        Listen two AI models talk to each other
+                    </h1>
 
-            <Image
-                src={waveformIllustration}
-                alt="Abstract waveform illustration covered with neurons"
-                className="mx-auto mt-10"
-            />
+                    <p className="leading-7 [&:not(:first-child)]:mt-4 text-center text-muted-foreground">
+                        And other fun AI-related things
+                    </p>
+
+                    <Button className="mt-6" size="lg">
+                        <PlayIcon /> Start
+                    </Button>
+                </div>
+
+                <WaveformIllustration />
+            </section>
         </main>
     )
 }
---
+++ src/app/(home)/waveform-illustration.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import waveformIllustration from '@/assets/images/waveform-illustration.svg'
+
+import { motion } from 'motion/react'
+
+export default function WaveformIllustration() {
+    return (
+        <div className='max-w-full overflow-hidden'>
+            <motion.img
+                initial={{ opacity: 0, scaleY: 0 }}
+                animate={{ opacity: 1, scaleY: 1, dur: 3000 }}
+                className="z-10 w-full max-w-xl mx-auto min-w-lg"
+                src={waveformIllustration.src}
+            />
+        </div>
+    )
+}
