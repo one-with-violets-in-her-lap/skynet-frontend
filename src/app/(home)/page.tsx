cc53a628eea76224617173f3deb3c8479b72b230
crucials <ensueness@gmail.com>
2025-03-01 18:03:21 +0500

    hero section: increase UI size on desktop

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
             <section className="relative pb-10">
                 <div className="bg-background/50 backdrop-blur-[3px] absolute w-full h-full top-0 left-0 z-20"></div>
 
-                <div className="relative p-6 mb-6 z-30 flex items-center justify-center flex-col">
+                <div className="relative p-6 mb-6 z-30 flex items-center justify-center flex-col lg:p-10">
                     <h1 className="scroll-m-20 text-4xl text-center font-extrabold tracking-tight lg:text-5xl">
                         Listen two AI models talk to each other
                     </h1>
