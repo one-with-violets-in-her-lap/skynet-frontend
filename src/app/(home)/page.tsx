810012803e5f970c7bdfd967939d3ad4ad518b78
crucials <ensueness@gmail.com>
2025-03-02 09:55:06 +0500

    hero section: replace waveform image, add start button

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,29 @@
+'use client'
+
 import { PlayIcon } from 'lucide-react'
+import { useState } from 'react'
+import { AnimatePresence, motion } from 'motion/react'
 import { Button } from '@/components/ui/button'
 import WaveformIllustration from '@/app/(home)/waveform-illustration'
 
 export default function Home() {
+    const [llmConversationRequested, setLlmConversationRequested] = useState(false)
+
+    const backdropClasses =
+        'transition-all duration-500 delay-300 bg-background/50 backdrop-blur-[2px] absolute w-full h-full top-0 left-0 z-20'
+
     return (
         <main>
             <section className="relative pb-10">
-                <div className="bg-background/50 backdrop-blur-[3px] absolute w-full h-full top-0 left-0 z-20"></div>
+                <div
+                    className={
+                        backdropClasses +
+                        ' ' +
+                        (llmConversationRequested
+                            ? 'backdrop-blur-none bg-transparent'
+                            : '')
+                    }
+                ></div>
 
                 <div className="relative p-6 mb-6 z-30 flex items-center justify-center flex-col lg:p-10">
                     <h1 className="scroll-m-20 text-4xl text-center font-extrabold tracking-tight lg:text-5xl">
@@ -17,9 +34,23 @@ export default function Home() {
                         And other fun AI-related things
                     </p>
 
-                    <Button className="mt-6" size="lg">
-                        <PlayIcon /> Start
-                    </Button>
+                    <AnimatePresence mode="popLayout">
+                        {!llmConversationRequested && (
+                            <motion.div
+                                key="start-llm-conversation-button"
+                                exit={{ scale: 0, opacity: 0 }}
+                                transition={{ duration: 0.3 }}
+                            >
+                                <Button
+                                    className="mt-6"
+                                    size="lg"
+                                    onClick={() => setLlmConversationRequested(true)}
+                                >
+                                    <PlayIcon /> Start
+                                </Button>
+                            </motion.div>
+                        )}
+                    </AnimatePresence>
                 </div>
 
                 <WaveformIllustration />
--- src/app/(home)/waveform-illustration.tsx
+++ src/app/(home)/waveform-illustration.tsx
@@ -6,10 +6,14 @@ import { motion } from 'motion/react'
 
 export default function WaveformIllustration() {
     return (
-        <div className='max-w-full overflow-hidden'>
+        <div className="max-w-full overflow-hidden">
             <motion.img
                 initial={{ opacity: 0, scaleY: 0 }}
-                animate={{ opacity: 1, scaleY: 1, dur: 3000 }}
+                animate={{ opacity: 1, scaleY: 1 }}
+                transition={{
+                    ease: 'easeInOut',
+                    duration: 0.5,
+                }}
                 className="z-10 w-full max-w-xl mx-auto min-w-lg"
                 src={waveformIllustration.src}
             />
