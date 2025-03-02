9dd27649b88c10d9bc262e13ac55111337201763
crucials <ensueness@gmail.com>
2025-03-02 10:24:43 +0500

    hero section: add noise lighting on the background

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,20 +1,23 @@
 'use client'
 
+import noisePurpleLightingImage from '@/assets/images/noise-purple-lighting.png'
+
 import { PlayIcon } from 'lucide-react'
 import { useState } from 'react'
 import { AnimatePresence, motion } from 'motion/react'
 import { Button } from '@/components/ui/button'
 import WaveformIllustration from '@/app/(home)/waveform-illustration'
+import Image from 'next/image'
 
 export default function Home() {
     const [llmConversationRequested, setLlmConversationRequested] = useState(false)
 
     const backdropClasses =
-        'transition-all duration-500 delay-300 bg-background/50 backdrop-blur-[2px] absolute w-full h-full top-0 left-0 z-20'
+        'transition-all duration-500 delay-300 bg-background/30 backdrop-blur-[1px] absolute w-full h-full top-0 left-0 z-20'
 
     return (
         <main>
-            <section className="relative pb-10">
+            <section className="relative pb-10 overflow-x-hidden">
                 <div
                     className={
                         backdropClasses +
@@ -25,7 +28,7 @@ export default function Home() {
                     }
                 ></div>
 
-                <div className="relative p-6 mb-6 z-30 flex items-center justify-center flex-col lg:p-10">
+                <div className="relative p-6 mb-12 z-30 flex items-center justify-center flex-col lg:p-10">
                     <h1 className="scroll-m-20 text-4xl text-center font-extrabold tracking-tight lg:text-5xl">
                         Listen two AI models talk to each other
                     </h1>
@@ -54,6 +57,12 @@ export default function Home() {
                 </div>
 
                 <WaveformIllustration />
+
+                <Image
+                    src={noisePurpleLightingImage}
+                    alt="Background lighting with noise"
+                    className="absolute bottom-20 min-w-xs left-1/2 -translate-x-1/2 sm:bottom-2"
+                />
             </section>
         </main>
     )
